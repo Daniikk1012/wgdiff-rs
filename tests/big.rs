cc4e577db5497af35e45e8450ed3509c8b6d299b
Daniikk1012 <daniyarsar@gmail.com>
2021-11-09 22:19:44 +0600

    Add tests for big amounts of data

---
+++ tests/big.rs
@@ -0,0 +1,29 @@
+use rand::prelude::*;
+use wgdiff::{Diff, Patch};
+
+fn bytes(length: usize) {
+    let mut old = vec![0; length];
+    let mut new = vec![0; length];
+
+    let mut rng = SmallRng::from_entropy();
+    rng.fill_bytes(&mut old);
+    rng.fill_bytes(&mut new);
+
+    old.patch(new.diff(&old));
+    assert_eq!(old, new);
+}
+
+#[test]
+fn bytes_1_000() {
+    bytes(1_000);
+}
+
+#[test]
+fn bytes_5_000() {
+    bytes(5_000);
+}
+
+#[test]
+fn bytes_10_000() {
+    bytes(10_000);
+}
